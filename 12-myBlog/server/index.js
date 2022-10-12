9f2b7f5044ba2b4f777dfd24a11127ae725ad920
Alma Benitez <alma.bntz@gmail.com>
2022-10-12 15:07:57 -0500

    added post to my back end

--- 12-myBlog/server/index.js
+++ 12-myBlog/server/index.js
@@ -1,31 +1,52 @@
 import express from 'express';
 import cors from 'cors';
 import pgPromise from 'pg-promise';
-import bodyParser from "body-parser";
+import bodyparser from "body-parser";
 
 const app = express();
 const PORT = 8080;
 const pgp = pgPromise({});
 
 //this is how i port my db over
-const db = pgp('postgres://localhost:5432/game');
+const db = pgp('postgres://localhost:5432/myblog');
 
 //middle wear  
 app.use(cors());
+app.use(bodyparser.urlencoded({extended: false}))
+app.use(bodyparser.json())
 
-// GET from players
-//this will be under http://localhost:8080/players
-app.get('/players', async function (req, res, next) {
+// GET from table myposts
+//this will be under http://localhost:8080/myposts
+app.get('/myposts', async function (req, res, next) {
 
     try {
-      const players = await db.any('SELECT * FROM players', [true]);
-      res.send(players);
+      const myPosts = await db.any('SELECT * FROM myposts', [true]);
+      res.send(myPosts);
     } catch (e) {
       return res.status(400).json({ e });
     }
   });
 
-
+// POST for myposts
+//this data is interacting with my Home and addPost conponents in the front end
+app.post("/myposts", async (req,res) => {
+  const blogPost = {
+    title: req.body.title,
+    img: req.body.img,
+    body: req.body.body,
+  };
+  console.log( blogPost);
+  try {
+    const createBlogPost = await db.one (
+      "INSERT INTO myposts(title, img, body) VALUES($1, $2, $3) RETURNING *",
+      [blogPost.title, blogPost.img, blogPost.body]
+    );
+    console.log(createBlogPost);
+    res.send(createBlogPost);
+  } catch (e) {
+    return res.status(400).json( {e} )
+  }
+});
 
 
 
